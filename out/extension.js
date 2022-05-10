3192651f1bec3c080d33c140879beb2a219a6e5d
Peter Heinemann <peter.heinemann@flowsquad.io>
2022-05-10 16:54:29 +0200

    remove unnecessary files

--- out/extension.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.activate = void 0;
-const jsonEditorProvider_1 = require("./jsonEditorProvider");
-function activate(context) {
-    context.subscriptions.push(jsonEditorProvider_1.JsonEditorProvider.register(context));
-}
-exports.activate = activate;
-//# sourceMappingURL=extension.js.map
